b760e91b075740b2fbae0bc1662b0149ffab5aee
Matt <mhur0724@gmail.com>
2021-11-28 11:45:40 -0800

    adding carousel

--- main.js
+++ main.js
@@ -32,17 +32,25 @@ function changeTheme() {
 let nintendoClick = 0;
 
 function nintendoPlay() {
-  if (nintendoClick < 3) {
-    audioCoin.currentTime = 0;
-    audioCoin.play();
-    nintendoClick++;
-    console.log("coin");
-  } else if (nintendoClick < 6) {
-    audio1Up.currentTime = 0;
-    audio1Up.play();
-    console.log("1up");
-    nintendoClick++;
-  } else {
-    return;
-  }
+  audioCoin.currentTime = 0;
+  audioCoin.play();
 }
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+    console.log(newIndex);
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
