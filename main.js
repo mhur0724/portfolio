6c8a3cbe85a8403e4ce78a30fdc073b08684dea7
Matt <mhur0724@gmail.com>
2021-12-01 10:29:44 -0800

    added hamburger menu

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-let theme = document.querySelector(".theme");
-let body = document.body;
-let header__ul = document.querySelector(".header__ul");
-let header__ul__li = document.querySelectorAll(".header__ul__li");
-let about__me = document.querySelector(".about__me");
-let about__me__intro = document.querySelector(".about__me__intro");
-let projects = document.querySelector(".projects");
-let project = document.querySelectorAll(".project");
-let nintendo = document.querySelector(".nintendo");
-let audioCoin = document.querySelector(".nintendo-audio-coin");
-let audio1Up = document.querySelector(".nintendo-audio-1up");
+const theme = document.querySelector(".theme__image");
+const body = document.body;
+const header__ul = document.querySelector(".header__ul");
+const header__ul__li = document.querySelectorAll(".header__ul__li");
+const about__me = document.querySelector(".about__me");
+const about__me__intro = document.querySelector(".about__me__intro");
+const projects = document.querySelector(".projects");
+const project = document.querySelectorAll(".project");
+const nintendo = document.querySelector(".nintendo");
+const audioCoin = document.querySelector(".nintendo-audio-coin");
+const audio1Up = document.querySelector(".nintendo-audio-1up");
+const menuBtn = document.querySelector(".menu-button");
+const menuBtnLines = document.querySelector(".menu-button__lines");
 
 theme.addEventListener("click", changeTheme);
 nintendo.addEventListener("click", nintendoPlay);
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("menu-button--open");
+  header__ul.classList.toggle("header__ul--open");
+});
 
 function changeTheme() {
   body.classList.toggle("body--dodgers");
@@ -19,6 +25,7 @@ function changeTheme() {
   header__ul__li.forEach((element) =>
     element.classList.toggle("header__ul__li--dodgers")
   );
+  menuBtnLines.classList.toggle("menu-button__lines--dodgers");
   about__me.classList.toggle("about__me--dodgers");
   about__me__intro.classList.toggle("about__me__intro--dodgers");
   projects.classList.toggle("projects--dodgers");
